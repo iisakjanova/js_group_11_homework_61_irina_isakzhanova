319490a10700e94b9507b291b53d8de5121440a3
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-08-20 14:37:03 +0600

    Changed API access key

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export const All_COUNTRIES_URL = BASE_URL + 'all?fields=name;alpha3Code';
 export const ONE_COUNTRY_URL = BASE_URL + 'alpha/';
 
 //API access key for https://coinlib.io/
-export const KEY_COINS = '49702aff19bd905b';
+export const KEY_COINS = 'f910f983bfab75c5';
 
 export const BASE_URL_COINS = 'https://coinlib.io/api/v1/coinlist';
 export const ALL_COINS_URL = BASE_URL_COINS + '?key=' + KEY_COINS;
